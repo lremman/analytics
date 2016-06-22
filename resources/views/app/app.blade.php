da812494fffa369eb76463f0e176fefb36792096
lremman <rom.loboda@gmail.com>
2016-06-22 11:46:38 +0300

    fix

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -5,7 +5,11 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <title>@section('title')RDEV ANALYTICS@show</title>
+        <title>
+            @section('title')
+                RDEV ANALYTICS
+            @show
+        </title>
 
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
